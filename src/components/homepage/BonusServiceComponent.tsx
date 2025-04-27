43ef5903a6aee8069110471135ce584505039ea5
Rasmi142 <rasmioff@gmail.com>
2025-04-27 15:03:46 +0530

    ui modified

--- src/components/homepage/BonusServiceComponent.tsx
+++ src/components/homepage/BonusServiceComponent.tsx
@@ -77,7 +77,7 @@ const BonusServiceComponent: React.FC = () => {
         <div className="max-w-5xl mx-auto">
           {/* "More = Free" Banner */}
           <div className="text-center mb-4">
-            <div className="inline-block px-4 py-2 bg-gradient-to-r from-emerald-600 to-teal-500 rounded-full transform -rotate-2">
+            <div className="inline-block px-4 py-2 bg-gradient-to-r from-emerald-600 to-teal-500 rounded-full ">
               <p className="text-lg sm:text-xl font-bold text-white">
                 {"Ohhh, wait, there's more. More = Free"}
               </p>
